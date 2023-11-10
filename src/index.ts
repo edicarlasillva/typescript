852a4bc2d02da4d9cb56134d55410a040dcb25bf
Carla Silva <carla.silva@movestock.com>
2023-11-09 21:32:46 -0300

    added new code

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,55 @@
-import { Usuario } from './classes/Usuario'
-import { Livro } from './classes/Livro'
+// import { Usuario } from './classes/Usuario'
+// import { Livro } from './classes/Livro'
 
-const usuario = new Usuario()
+// const usuario = new Usuario()
 
-console.log(usuario)
+// console.log(usuario)
 
-usuario.nome = 'Maria'
-usuario.idade = 35
+// usuario.nome = 'Maria'
+// usuario.idade = 35
 
-console.log(usuario)
+// console.log(usuario)
 
-usuario.saudacao()
+// usuario.saudacao()
 
-// Criar instância baseada na classe Livro
-const livro1 = new Livro()
+// // Criar instância baseada na classe Livro
+// const livro1 = new Livro()
 
-livro1.titulo = 'Roube como um artista'
-livro1.autor = 'Austin Kleon'
-livro1.anoPublicacao = 2015
+// livro1.titulo = 'Roube como um artista'
+// livro1.autor = 'Austin Kleon'
+// livro1.anoPublicacao = 2015
 
-livro1.mostrarDetalhes()
+// livro1.mostrarDetalhes()
 
-const livro2 = new Livro()
-livro2.titulo = 'A única coisa'
-livro2.autor = 'Gary'
-livro2.anoPublicacao = 2015
+// const livro2 = new Livro()
+// livro2.titulo = 'A única coisa'
+// livro2.autor = 'Gary'
+// livro2.anoPublicacao = 2015
 
-livro2.mostrarDetalhes()
+// livro2.mostrarDetalhes()
+
+
+// Construtores
+class Usuario {
+  nome: string
+  idade: number
+  email: string
+
+  // construtor
+  constructor(nome: string, idade: number, email?: string) {
+    this.nome = nome
+    this.idade = idade
+    this.email = email
+  }
+
+  saudacao(): void {
+    const mensagem = `Olá, meu nome é ${this.nome}, tenho ${this.idade} anos${this.email ? `e meu e-mail é ${this.email}. ` : ` 'email não informado.'`}`
+
+    console.log(mensagem)
+  }
+}
+
+const usuario = new Usuario('Carla', 35)
+console.log(usuario.saudacao())
+
+// Crie uma classe ContaBancaria com propriedades titular, saldo e um método sacar que reduza o saldo. Adicione um construtor que aceite o titular e um saldo inicial (com um saldo padrão de 0). Crie uma instância da ContaBancaria, realize alguns saques e exiba o saldo final.
